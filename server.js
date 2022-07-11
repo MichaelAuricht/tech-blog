2a358a9acd474f2e084fa57fec18db61a055cfa9
Michael Auricht <michaelauricht@live.com.au>
2022-07-11 21:32:49 +1000

    added handlebars and other connection parts

--- server.js
+++ server.js
@@ -1,44 +1,31 @@
-const path = require("path");
-const express = require("express");
-const session = require("express-session");
-const exphbs = require("express-handlebars");
-
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-const sequelize = require("./config/connection.js");
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers')
+const sequelize = require('./config/connection')
+const session = require('express-session')
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const app = express();
+const PORT = process.env.PORT || 3002;
+const hbs = exphbs.create();
 
-const sess = {
-  secret: "Super secret secret",
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
-};
-
-app.use(session(sess));
-
-const hbs = exphbs.create({
-  helpers: {
-    format_date: date => {
-      return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
-    }
-  }
-});
-
-app.engine("handlebars", hbs.engine);
-app.set("view engine", "handlebars");
-
+app.use(session({
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    maxAge: 60000,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+}))
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(routes);
 
-app.use(require('./controllers/'));
-
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
-  sequelize.sync({ force: false });
-});
+sequelize.sync({force: false}).then(() =>  {
+    app.listen(process.env.PORT || 3002, () => console.log(`listening on http://localhost:${PORT}`))
+})
