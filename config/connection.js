4c32ddc1adf60b0cb110b1627b6261e8f415a631
Michael Auricht <michaelauricht@live.com.au>
2022-07-12 17:56:18 +1000

    trying again with heroku

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ let sequelize = new Sequelize(
         username: process.env.DB_USER,
         password: process.env.DB_PW,
         database: process.env.DB_NAME,
-        host: 'https://git.heroku.com/ma-tech-blog.git',
+        host: 'localhost',
         dialect: 'mysql',
         port: 3306,
     });
